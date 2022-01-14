1755908fca20b842dc3541ef3121e997254bec3e
SusanBae <90326272+devSusan@users.noreply.github.com>
2022-01-14 16:46:21 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const className = ["bg_purple", "bg_yellow", "bg_blue"];
 const doc = document.body;
 const minWidth = window.innerWidth;
 const maxWidth = minWidth + 300;
@@ -5,25 +6,26 @@ const maxWidth = minWidth + 300;
 const eventHandler = {
   handleResize: function () {
     const currentWidth = window.innerWidth;
+    let bg_color;
+    removeClassAllColor();
+
     if (currentWidth > minWidth && currentWidth < maxWidth) {
-      removeClassAllColor();
-      addClassColor("bg_purple");
+        bg_color = className[0];
     } else if (window.innerWidth > maxWidth) {
-      removeClassAllColor();
-      addClassColor("bg_yellow");
+        bg_color = className[1];
     } else if (window.innerWidth <= minWidth) {
-      removeClassAllColor();
-      addClassColor("bg_blue");
+        bg_color = className[2];
     }
+    addClass(bg_color);
   }
 };
 
 function removeClassAllColor() {
-  doc.classList.remove("bg_purple", "bg_yellow", "bg_blue");
+  doc.classList.remove(...className);
 }
 
-function addClassColor(color) {
-  doc.classList.add(color);
+function addClass(bg_color) {
+  doc.classList.add(bg_color);
 }
 
 window.addEventListener("resize", eventHandler.handleResize);
